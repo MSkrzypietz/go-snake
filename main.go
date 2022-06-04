2b0cc43a266165d78f6a494d8359b391604a2067
MSkrzypietz <michael.skr97@gmail.com>
2022-06-04 16:55:29 +0200

    add basic snake movement

--- main.go
+++ main.go
@@ -13,27 +13,89 @@ const (
 
 type frameMsg time.Time
 
+type Direction int
+
+const (
+	North Direction = iota
+	West
+	South
+	East
+)
+
+type head struct {
+	x int
+	y int
+}
+
+type snake struct {
+	head          head
+	currDirection Direction
+}
+
+func (s *snake) move() {
+	switch s.currDirection {
+	case North:
+		s.head.y--
+	case West:
+		s.head.x++
+	case South:
+		s.head.y++
+	case East:
+		s.head.x--
+	}
+}
+
 type model struct {
+	snake snake
 }
 
 func initialModel() model {
-	return model{}
+	return model{
+		snake: snake{
+			head:          head{x: 2, y: 2},
+			currDirection: South,
+		},
+	}
 }
 
 func (m model) View() string {
-	return ""
+	s := ""
+	for row := 0; row < 30; row++ {
+		for col := 0; col < 10; col++ {
+			if m.snake.head.x == col && m.snake.head.y == row {
+				s += "X"
+			} else {
+				s += " "
+			}
+		}
+		s += "\n"
+	}
+	return s
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
 	case frameMsg:
+		m.snake.move()
 		return m, animate()
 
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
+		case "w":
+			m.snake.currDirection = North
+			return m, nil
+		case "d":
+			m.snake.currDirection = West
+			return m, nil
+		case "s":
+			m.snake.currDirection = South
+			return m, nil
+		case "a":
+			m.snake.currDirection = East
+			return m, nil
 		}
 	}
 
